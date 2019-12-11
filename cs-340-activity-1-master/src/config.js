de81290a97ee24339d9f7b29ac83d59b95c96e4b
thompsoq <thompsoq@oregonstate.edu>
2019-12-11 14:25:29 -0800

    Updated comments on config.js

--- cs-340-activity-1-master/src/config.js
+++ cs-340-activity-1-master/src/config.js
@@ -1,11 +1,5 @@
-/**
- * TODO: update the configuration in this file by changing the user, password, dbname, and onid fields:
- *
- * Change all instances of "username" to your ONID and "XXXXX" to your MySQL DB password (the same one you use to log
- * in to phpMyAdmin)
- *
- * DISCLAIMER: it is not best practice to check configuration into source control (like GitHub). This is done here
- * merely to simplify the setup process.
+/*
+ * Handles information to log into phpMyAdmin database
  */
 module.exports = {
     host: 'classmysql.engr.oregonstate.edu',
