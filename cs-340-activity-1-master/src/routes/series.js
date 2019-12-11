23884fc943f2e6eb37ed1b520ae5ab2a9bef525a
thompsoq <thompsoq@oregonstate.edu>
2019-12-11 15:11:43 -0800

    Updated series.js comments

--- cs-340-activity-1-master/src/routes/series.js
+++ cs-340-activity-1-master/src/routes/series.js
@@ -1,13 +1,15 @@
+/*
+ * A lot of this page uses parts from activity 1 as a guide. Begin Express middleware and route
+ */
 const express = require('express');
 const { createViewContext } = require('../utils');
 
 const router = express.Router();
 
-/**
- * Route for listing the catalog of parts.
- * 
- * This serves as an example of joining tables to produce more complex queries. You do not need to modify anything
- * in this file.
+/*
+ * Route to actors database table, query for series id, series title, the initial air date, contributing writers 
+ * number of sesies, series end date, and network name, then create
+ * view/render for series.hbl if there were no errors 
  */
 router.get('/Series', (req, res, next) => {
     req.db.query(
@@ -31,20 +33,18 @@ router.get('/Series', (req, res, next) => {
 });
 
 
-/**
- * Route for displaying the form used to add a new part supplier.
+/*
+ * Route to display the series search page
  */
 router.get('/series/search', (req, res) => {
     res.render('series-search', createViewContext({ message: 'Search for a Series' }));
 });
 
-/**
- * Logic for actually adding a new part supplier using data from a form submission.
+/*
+ * Script behind quering the series average rating
  */
 router.post('/series/search', (req, res, next) => {
     let context = createViewContext();
-
-    // Make sure a supplier with the provided SID doesn't already exist
     req.db.query(
 		`
 		SELECT s.title, AVG(r.rating)
